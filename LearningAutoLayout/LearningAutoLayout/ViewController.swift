f5823d1479c95f7db688decdbbd70c4c2c47808a
Nicholas Coggin <nicholascoggin@Nicholass-MacBook-Pro.local>
2019-02-01 05:10:16 -0600

    initial commit - project creation

---
+++ LearningAutoLayout/LearningAutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearningAutoLayout
+//
+//  Created by Nicholas Coggin on 2/1/19.
+//  Copyright © 2019 Nicholas Coggin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
